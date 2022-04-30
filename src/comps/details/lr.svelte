241c3bbc1f9648b541346d3e50cc2e9de6969ac6
Bepely <ngogin7@gmail.com>
2022-04-30 12:42:52 +0300

    Фикс кнопок LR
    Фиксанул кривые кнопки

--- src/comps/details/lr.svelte
+++ src/comps/details/lr.svelte
@@ -10,15 +10,13 @@ import Button from "./button.svelte"
 
 
 let right = (user_store) => {
-    if (user_store.current_point_counter < user_store.points.length - 1) {
-        user_store.current_point_counter = user_store.current_point_counter + 1
-        user_store.current_point = user_store.points[user_store.current_point_counter]
-        user_store.screen_emit = user_store.screen_emit++
-        console.log("right 1", $doer_store.current_point_counter, "of", $doer_store.points.length - 1);
-    } else if (user_store.current_point_counter >= user_store.points.length - 1){
+    if (user_store.current_point_counter >= user_store.points.length - 1) {
         user_store.current_point_counter = 0
         user_store.current_point = user_store.points[user_store.current_point_counter]
-        user_store.screen_emit = 0
+        console.log("right 1", $doer_store.current_point_counter, "of", $doer_store.points.length - 1);
+    } else {
+        user_store.current_point_counter = user_store.current_point_counter + 1
+        user_store.current_point = user_store.points[user_store.current_point_counter]
         console.log("right 2", user_store.current_point_counter, "of", user_store.points.length - 1);
     }
     $doer_store = user_store
@@ -26,14 +24,15 @@ let right = (user_store) => {
 }
 
 let left = (user_store) => {
-    if (user_store.current_point_counter < user_store.points.length - 1) {
+    if (user_store.current_point_counter <= 0) {
         user_store.current_point_counter = user_store.points.length - 1
         user_store.current_point = user_store.points[user_store.current_point_counter]
-        console.log("left 1", $doer_store.current_point_counter, "of", $doer_store.points.length - 1);
-    } else if (user_store.current_point_counter >= user_store.points.length - 1){
+        console.log("left 2", user_store.current_point_counter, "of", user_store.points.length - 1);
+
+    } else {
         user_store.current_point_counter = user_store.current_point_counter - 1
         user_store.current_point = user_store.points[user_store.current_point_counter]
-        console.log("left 2", user_store.current_point_counter, "of", user_store.points.length - 1);
+        console.log("left 1", $doer_store.current_point_counter, "of", $doer_store.points.length - 1);
     }
     
     $doer_store = user_store
